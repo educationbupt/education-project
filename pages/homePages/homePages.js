bdd67af7a40bede9a4aa7cc2f95c3cc6aad314c5
ziyanwang <756474646@qq.com>
2019-03-02 17:39:36 +0800

    Main structure and first pages.

---
+++ pages/homePages/homePages.js
@@ -0,0 +1,76 @@
+// pages/homePages/homePages.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+  data: {
+    newMassage: [
+      { content: "新闻1", picture: "/image/1.jpg" },
+      { content: "新闻2", picture: "/image/2.jpg" },
+      { content: "新闻3", picture: "/image/3.jpg" }
+    ],
+    imgUrls:[
+      "/image/1.jpg",
+      "/image/2.jpg",
+      "/image/3.jpg"
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
